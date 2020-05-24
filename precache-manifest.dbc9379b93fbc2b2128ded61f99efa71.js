7dea91d2329cfa917e267077f7eb2acfab6e5a04
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-24 11:42:03 -0300

    Updates

---
+++ precache-manifest.dbc9379b93fbc2b2128ded61f99efa71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f544a41be42fa4aebcd62d19030889cc",
+    "url": "/juantsapp/index.html"
+  },
+  {
+    "revision": "27df5583e9e9d65d4ac1",
+    "url": "/juantsapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6cd36adb20c5416d720",
+    "url": "/juantsapp/static/js/2.429af135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/juantsapp/static/js/2.429af135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27df5583e9e9d65d4ac1",
+    "url": "/juantsapp/static/js/main.53afdf2c.chunk.js"
+  },
+  {
+    "revision": "6879d99d3df77ad8040d",
+    "url": "/juantsapp/static/js/runtime-main.62e77619.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/juantsapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
