984c77142d6b585dcaf313585b2ca046c67a9591
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-26 14:10:54 -0300

    hover profile photo

--- src/components/Directs/Profile/index.js
+++ src/components/Directs/Profile/index.js
@@ -1,30 +1,28 @@
-import React, {useContext} from "react";
-import Stat from 'components/Directs/Stat';
-import Context from 'components/Context/AppContext';
+import React, { useContext } from "react";
+import Stat from "components/Directs/Stat";
+import Context from "components/Context/AppContext";
+import camera from 'img/camera.svg';
 import "./css.css";
 import setUser from "components/services/setUser";
 
 export default function Profile(props) {
+  const { users, setUsers } = useContext(Context);
 
-  const {users, setUsers} = useContext(Context);
-
-  const editName = name => {
+  const editName = (name) => {
     var edit_user = props.user;
-    if(name)
-      edit_user.name = name;
+    if (name) edit_user.name = name;
     return editUser(edit_user);
-  }
+  };
 
-  const editDesc = desc => {
+  const editDesc = (desc) => {
     var edit_user = props.user;
-    if(desc)
-      edit_user.desc = desc;
+    if (desc) edit_user.desc = desc;
     return editUser(edit_user);
-  }
+  };
 
   const editUser = (user) => {
     return setUser(user, users, setUsers);
-  }
+  };
 
   return (
     <div className="content-new-chat">
@@ -38,18 +36,23 @@ export default function Profile(props) {
         </div>
       </header>
       <div className="content-profile-stats">
-        <div className="center-stat">
+        <div className="center-stat content-profile-img">
           <img
             src={props.user.picture}
             alt="profile-img"
             className="profile-img"
           />
+          <div className="hover-img">
+            <img src={camera} alt="camera" className="stat-camera-hover" />
+            <p className="stat-text-hover">CAMBIAR FOTO DE PERFIL</p>
+          </div>
         </div>
         <Stat edit={editName} title="Tu nombre" desc={props.user.name} />
         <div className="center-stat">
-            <p className="info-stat">
-                Este no es tu nombre de usuario ni un PIN. Este nombre será visible para tus contactos de JuantsApp.
-            </p>
+          <p className="info-stat">
+            Este no es tu nombre de usuario ni un PIN. Este nombre será visible
+            para tus contactos de JuantsApp.
+          </p>
         </div>
         <Stat edit={editDesc} title="Info." desc={props.user.desc} />
       </div>
