e176fb924600f0e4dd596f5c6d76b7aa8bb95858
Pupi Marti <42395985+pupimarti@users.noreply.github.com>
2020-05-27 09:53:18 -0300

    fix bugs css + detect dark mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, {useState} from 'react';
-import Directs from './components/Directs'
+import React, { useState } from "react";
+import Directs from "./components/Directs";
 import { AppContextProvider } from "components/Context/AppContext";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-
-  const [dark, setDark] = useState(false);
+  const [dark, setDark] = useState(
+    (window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches) ||
+      false
+  );
 
   const handleSetDark = () => {
     setDark(!dark);
-  }
+  };
 
   return (
     <div className={dark ? "content-app dark" : "content-app"}>
       <div className="header-app"></div>
       <div className="app">
         <AppContextProvider>
-            <Directs setMode={handleSetDark} />
-          </AppContextProvider>
+          <Directs setMode={handleSetDark} />
+        </AppContextProvider>
       </div>
     </div>
   );
